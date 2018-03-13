a2307c51c263fd77bfaabfc5253e05cd09e9e513
chernetsky <chernetsky@gmail.com>
2018-03-13 12:28:10 +0800

    examples, dist, license

--- bin/build_release.sh
+++ bin/build_release.sh
@@ -5,3 +5,4 @@ sh ./bin/build.sh all
 cp ./out/chart-editor.css ./dist/chart-editor.css
 cp ./out/chart-editor.min.css ./dist/chart-editor.min.css
 cp ./out/chart-editor.min.js ./dist/chart-editor.min.js
+cp ./out/deps.js ./dist/deps.js
