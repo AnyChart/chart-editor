be4ecc00cebeaf07fd8647c796f2caf90a8e2e24
chernetsky <chernetsky@gmail.com>
2018-03-20 12:34:15 +0800

    build & examples impr

--- bin/build_release.sh
+++ bin/build_release.sh
@@ -5,4 +5,3 @@ sh ./bin/build.sh all
 cp ./out/chart-editor.css ./dist/chart-editor.css
 cp ./out/chart-editor.min.css ./dist/chart-editor.min.css
 cp ./out/chart-editor.min.js ./dist/chart-editor.min.js
-cp ./out/deps.js ./dist/deps.js
